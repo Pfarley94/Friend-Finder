be8ddf5e2820f25ba424226b07d1ec683e7af8f9
Patrick Farley <patrickfarley1994@yahoo.com>
2018-05-03 01:10:24 -0400

    added code, still having crossorigin error

--- app/server.js
+++ app/server.js
@@ -1,14 +1,31 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require("path");
 
 
 
 
 
 var app = express()
- 
+var PORT = 80;
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+
+require(path.join(__dirname, './routing/apiRoutes.js'))(app);
+require(path.join(__dirname, './routing/htmlRoutes.js'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
